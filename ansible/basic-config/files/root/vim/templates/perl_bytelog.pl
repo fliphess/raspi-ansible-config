105e5f73d99993cf8514159024375845365d0be6
Flip <flip@byte.nl>
2014-06-30 13:56:25 +0200

    remove template

--- ansible/basic-config/files/root/vim/templates/perl_bytelog.pl
+++
@@ -1,6 +0,0 @@
-use Byte::Log;
-my $log = new Byte::Log();
-$log->class('dbigration/activation/database backup/....');
-$log->tag('cancelled/requested/started/finished');
-$log->source('quickscan/sevicepanel/...');
-$log->log($domain,"this happened");
